1efe1bb4db6f4be372ed501e7143aa5e57cc16ad
grekz <vidbla.grekz@gmail.com>
2018-02-07 08:57:06 +0100

    JSLCE-104. Maximum Depth of Binary Tree

---
+++ mx/grekz/leetcode/easy/E104_MaximumDepthofBinaryTree.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root == null) return 0;
+    const left = maxDepth(root.left);
+    const right = maxDepth(root.right);
+    return ( left > right ? left : right ) + 1 ;
+};
