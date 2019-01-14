06bba84528fd08424b26125b83287544d027a5b0
Grekz <vidbla.grekz@gmail.com>
2019-01-14 08:07:54 +0100

    JSLCE-701. Insert into a Binary Search Tree

---
+++ mx/grekz/leetcode/medium/E701_InsertintoaBinarySearchTree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function (root, val) {
+    if (!root) return new TreeNode(val)
+    if (val < root.val) root.left = insertIntoBST(root.left, val)
+    else root.right = insertIntoBST(root.right, val)
+    return root
+};
