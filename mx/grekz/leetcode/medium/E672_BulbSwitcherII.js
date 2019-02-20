2a33cddaa6ab80caf4c1e129aba1619979e51264
Grekz <vidbla.grekz@gmail.com>
2019-02-20 12:49:39 +0100

    JSLCE-672. Bulb Switcher II

---
+++ mx/grekz/leetcode/medium/E672_BulbSwitcherII.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var flipLights = function (n, m) {
+    if (m == 0) return 1;
+    if (n == 1) return 2;
+    if (n == 2 && m == 1) return 3;
+    if (n == 2 || m == 1) return 4;
+    if (m == 2) return 7;
+    if (m >= 3) return 8;
+    return 8;
+};
