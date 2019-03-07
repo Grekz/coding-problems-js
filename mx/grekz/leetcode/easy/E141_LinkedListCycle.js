34300852ebcd2417f021479f71e53a163f7f32d9
Grekz <vidbla.grekz@gmail.com>
2019-03-07 21:22:26 +0100

    JSLCE-141. Linked List Cycle

--- mx/grekz/leetcode/easy/E141_LinkedListCycle.js
+++ mx/grekz/leetcode/easy/E141_LinkedListCycle.js
@@ -11,13 +11,12 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let slow = head, fast = head;
-    while (fast != null && fast.next != null){
-        slow = slow.next;
-        fast = fast.next.next;
-        if (slow == fast) {
-            return true;
-        }
+    let slow = head, fast = head
+    while ( fast != null && fast.next != null ) {
+        slow = slow.next
+        fast = fast.next.next
+        if ( slow == fast ) 
+            return true
     }
-    return false;
+    return false
 };
